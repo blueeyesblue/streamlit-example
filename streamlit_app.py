65ab3a4cbb862c1e4d96756de1bf363483aa2cef
Chuck Shen <blueeyesblue78@gmail.com>
2023-08-10 00:25:34 +0800

    Update streamlit_app.py
    update code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,88 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
+###############
+# Authored by Weisheng Jiang
+# Book 4  |  From Basic Arithmetic to Machine Learning
+# Published and copyrighted by Tsinghua University Press
+# Beijing, China, 2022
+###############
+
 import streamlit as st
+import numpy as np
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
 
-"""
-# Welcome to Streamlit!
+def bmatrix(a):
+    """Returns a LaTeX bmatrix
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    :a: numpy array
+    :returns: LaTeX bmatrix as a string
+    """
+    if len(a.shape) > 2:
+        raise ValueError('bmatrix can at most display two dimensions')
+    lines = str(a).replace('[', '').replace(']', '').splitlines()
+    rv = [r'\begin{bmatrix}']
+    rv += ['  ' + ' & '.join(l.split()) + r'\\' for l in lines]
+    rv +=  [r'\end{bmatrix}']
+    return '\n'.join(rv)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#%%
+with st.sidebar:
+    
+    num_a = st.slider('Number of rows, a:',
+              3,6,step = 1)
+    num_b = st.slider('Number of rows, b:',
+              3,6,step = 1)
+    
+a = np.random.uniform(0,1,num_a).reshape((-1,1))
+a = np.round(a,1)
+b = np.random.uniform(0,1,num_b).reshape((-1,1))
+b = np.round(b,1)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+show_number = False
+with st.sidebar:
+    show_number = st.checkbox('Display values')
+
+tensor_a_b = a@b.T
+
+#%% visualization
+
+st.latex('a = ' + bmatrix(a))
+st.latex('b = ' + bmatrix(b))
+st.latex('a \otimes b = ab^{T}')
+st.latex( bmatrix(a) + '@' + bmatrix(b.T) + ' = ' + bmatrix(tensor_a_b))
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    fig_a = px.imshow(a, text_auto=show_number, 
+                  color_continuous_scale='viridis',
+                  aspect = 'equal')
+    
+    fig_a.update_layout(height=400, width=300)
+    fig_a.layout.coloraxis.showscale = False
+    st.plotly_chart(fig_a)
+
+with col2:
+    fig_b = px.imshow(b, text_auto=show_number, 
+                  color_continuous_scale='viridis',
+                  aspect = 'equal')
+    
+    fig_b.update_layout(height=400, width=300)
+    fig_b.layout.coloraxis.showscale = False
+    st.plotly_chart(fig_b)
+
+with col3:
+    fig_ab = px.imshow(tensor_a_b, text_auto=show_number, 
+                  color_continuous_scale='viridis',
+                  aspect = 'equal')
+    
+    fig_ab.update_layout(height=400, width=400)
+    
+    fig_ab.layout.coloraxis.showscale = False
+    st.plotly_chart(fig_ab)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
